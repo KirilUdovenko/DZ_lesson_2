e04e2c63b3a1b99124ca1400414397af30e91646
KirilUdovenko <kiril.stuff1@gmail.com>
2021-10-05 21:02:28 +0300

    DZ po Lesson 2 / v.2

--- DZ_lesson_2.py
+++ DZ_lesson_2.py
@@ -17,13 +17,13 @@
 #     print("com not in www")
 #
 # www = 'www.command_and_conquer.net'
-# if "com" in www:
+# if ".com" in www:
 #     print("com in www")
 # else:
 #     print("com not in www")
 #
 # www = 'www.conquer_and_command.net'
-# if "com" in www:
+# if ".com" in www:
 #     print("com in www")
 # else:
 #     print("com not in www")
